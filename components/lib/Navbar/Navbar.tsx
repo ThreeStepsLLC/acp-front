40227c94cb0a53065622c3e99d1e04a786cabf8d
Oktay Afandi <ogtayaa@code.edu.az>
2023-07-28 22:43:38 +0400

    feats

--- components/lib/Navbar/Navbar.tsx
+++ components/lib/Navbar/Navbar.tsx
@@ -52,7 +52,6 @@ export default function Example() {
           </Link>
         </Popover.Group>
       </nav>
-      <hr />
 
       <Dialog
         as="div"
--- components/lib/WelcomeSection/WelcomeSection.tsx
+++ components/lib/WelcomeSection/WelcomeSection.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 const WelcomeSection = () => {
   return (
     <div className="bg-[#001F3F]">
-      <div className="container mx-auto px-16 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 pb-40">
-        <div className="lg:py-20 lg:pt-40 sm:py-20 sm:pt-20 py-20 pt-20">
+      <div className="container mx-auto px-16 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 pb-20">
+        <div className="lg:py-20 lg:pt-20 sm:py-20 sm:pt-20 py-20 pt-20">
           <p className="text-[18px] font-semibold text-[#81a32b]">Professional. Innovative. Reliable.</p>
           <p className="lg:text-[58px] text-[38px] font-extrabold text-[#ffffff]" >Exceptional <br /> Service Exceeding Expectations</p>
           <p className="text-[19px] font-normal text-[#ffffff]">Lorem ipsum dolor sit amet consectetur adipisicing <br /> elit. Sapiente velit, tenetur consequuntur accusamus voluptatum, <br /> ut itaque sunt quo ab debitis ad? Itaque voluptatem quae culpa. <br /></p>
         </div>
-        <div className="lg:pt-40 sm:pt-10 pt-10 gap-12">
+        <div className="lg:pt-20 sm:pt-10 pt-10 gap-12">
           <div className="grid text-end mb-20">
           <p className="text-[48px] font-bold text-[#81a32b]">128 +</p>
           <p className="text-[19px] font-bold text-[#ffffff]">Projects / YEAR</p>
