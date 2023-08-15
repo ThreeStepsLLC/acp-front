225ff1b50fd590087f40fc53577e0671d6fe81f6
Oktay Afandi <ogtayaa@code.edu.az>
2023-08-16 01:21:17 +0400

    feats

--- components/lib/LicencesSection/LicencesSection.tsx
+++ components/lib/LicencesSection/LicencesSection.tsx
@@ -4,18 +4,19 @@ import Image from "next/image";
 const LicencesSection = () => {
   return (
     <div className="container mx-auto px-12 py-16">
-                <p className="text-[#000000] text-[18px] font-semibold flex gap-4 mx-10">
+                <p className="text-[#000000] text-[18px] font-semibold flex gap-4">
           <Image
             src={"yellowLine.svg"}
             alt={"whatwedo"}
             height={1}
             width={40}
           />
-          CORE VALUES
+          LICENCES
         </p>
         <p className="text-[#000000] text-[45px] font-bold mb-8">
-        What are our{" "}
-        <span className="text-[#81a32b] text-[45px] font-bold">core values?</span>
+        What{' '}
+        <span className="text-[#81a32b] text-[45px] font-bold">licenses</span>
+        {' '}do we have?
       </p>
 
       <div className="lg:flex md:flex lg:justify-start md:justify-start justify-center grid gap-10">
