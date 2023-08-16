664365cdbbba8c3602f3251d79b324b3b225fd49
Oktay Afandi <ogtayaa@code.edu.az>
2023-08-16 21:48:30 +0400

    feats

--- components/lib/WelcomeSection/WelcomeSection.tsx
+++ components/lib/WelcomeSection/WelcomeSection.tsx
@@ -31,7 +31,7 @@ const WelcomeSection = () => {
     >
       <div className="container mx-auto px-12 grid lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1 pb-10 relative">
         <div className="lg:py-10 lg:pt-16 pt-40 py-0">
-          <p className="text-[#00517b] lg:text-[18px] md:text-[18px] text-[14px] font-semibold flex gap-4">
+          <p className="text-[#ffffff] lg:text-[18px] md:text-[18px] text-[14px] font-semibold flex gap-4">
             <Image
               src={"yellowLine.svg"}
               alt={"whatwedo"}
@@ -40,7 +40,7 @@ const WelcomeSection = () => {
             />
             Professional. Innovative. Reliable.
           </p>
-          <p className="lg:text-[40px] md:text-[40px] text-[25px] font-extrabold text-[#00517b] mb-4">
+          <p className="lg:text-[40px] md:text-[40px] text-[25px] font-extrabold text-[#ffffff] mb-4">
             The power of engineering, at your service
           </p>
           <p className="lg:text-[20px] md:text-[20px] text-[20px] font-semibold text-[#ffffff]">
@@ -62,7 +62,7 @@ const WelcomeSection = () => {
             <p className="lg:text-[40px] md:text-[40px] text-[20px] font-bold text-[#81a32b]">
               8
             </p>
-            <p className="lg:text-[19px] md:text-[19px] text-[16px] font-bold text-[#00517b]">
+            <p className="lg:text-[19px] md:text-[19px] text-[16px] font-bold text-[#ffffff]">
               Total Projects
             </p>
           </div>
@@ -70,23 +70,23 @@ const WelcomeSection = () => {
             <p className="lg:text-[40px] md:text-[40px] text-[20px] font-bold text-[#81a32b]">
               600km+
             </p>
-            <p className="lg:text-[19px] md:text-[19px] text-[16px] font-bold text-[#00517b]">
+            <p className="lg:text-[19px] md:text-[19px] text-[16px] font-bold text-[#ffffff]">
               Total Road
             </p>
           </div>
           <div className="grid lg:text-start md:text-start text-center mb-10">
             <p className="lg:text-[40px] md:text-[40px] text-[20px] font-bold text-[#81a32b]">
-              12
+             66424 m
             </p>
-            <p className="lg:text-[19px] md:text-[19px] text-[16px] font-bold text-[#00517b]">
+            <p className="lg:text-[19px] md:text-[19px] text-[16px] font-bold text-[#ffffff]">
               Total Tunnels
             </p>
           </div>
           <div className="grid lg:text-start md:text-start text-center mb-10">
             <p className="lg:text-[40px] md:text-[40px] text-[20px] font-bold text-[#81a32b]">
-              125
+             121
             </p>
-            <p className="lg:text-[19px] md:text-[19px] text-[16px] font-bold text-[#00517b]">
+            <p className="lg:text-[19px] md:text-[19px] text-[16px] font-bold text-[#ffffff]">
               Total Bridges
             </p>
           </div>
