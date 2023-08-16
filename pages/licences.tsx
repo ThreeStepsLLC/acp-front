31cfa10c70f8528c99eeb18b5bc51030198bba0b
Oktay Afandi <ogtayaa@code.edu.az>
2023-08-16 22:22:45 +0400

    feats

--- pages/licences.tsx
+++ pages/licences.tsx
@@ -1,18 +1,67 @@
-import Layout from '@/components/layout'
-import PageHeader from '@/components/lib/PageHeader/PageHeader'
-import React from 'react'
-import Image from "next/image";
+import Layout from '@/components/layout';
+import PageHeader from '@/components/lib/PageHeader/PageHeader';
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Modal from 'react-modal';
 
-const licences = () => {
+const Licences = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState('');
+
+  const openModal = (imageSrc: any) => {
+    setSelectedImage(imageSrc);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedImage('');
+    setModalIsOpen(false);
+  };
+
   return (
     <Layout title="Licences">
-      <PageHeader title={"Licences"} subTitle={"Our Licences"} />
+      <PageHeader title={'Licences'} subTitle={'Our Licences'} />
       <div className="container mx-auto px-16 py-16 lg:flex md:flex lg:justify-start md:justify-start justify-center grid gap-10">
-        <Image src={'/ACP Engineering 9001 2023-1.jpg'} alt={'iso'} width={200} height={200} />
-        <Image src={'/ACP Engineering 14001 2023-2.jpg'} alt={'iso'} width={200} height={200} />
+        <Image
+          src={'/ACP Engineering 9001 2023-1.jpg'}
+          alt={'iso'}
+          width={200}
+          height={200}
+          onClick={() => openModal('/ACP Engineering 9001 2023-1.jpg')}
+          className="clickable-image"
+          style={{cursor: 'pointer'}}
+        />
+        <Image
+          src={'/ACP Engineering 14001 2023-2.jpg'}
+          alt={'iso'}
+          width={200}
+          height={200}
+          onClick={() => openModal('/ACP Engineering 14001 2023-2.jpg')}
+          className="clickable-image"
+          style={{cursor: 'pointer'}}
+        />
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Image Modal"
+        className="image-modal"
+        style={{
+          content: {
+            maxWidth: '400px',
+            margin: 'auto',
+            marginTop: '20px',
+            border: '0px'
+          }
+        }}
+        onClick={closeModal}
+      >
+        {selectedImage && (
+          <Image src={selectedImage} alt="iso" width={800} height={800} />
+        )}
+      </Modal>
     </Layout>
-  )
-}
+  );
+};
 
-export default licences
+export default Licences;
