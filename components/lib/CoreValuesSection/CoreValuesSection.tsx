490f0008e25a677abd24c5d148a56fbff5120362
oktayafandi <ogtayaa@code.edu.az>
2023-08-31 11:52:45 +0400

    feats

--- components/lib/CoreValuesSection/CoreValuesSection.tsx
+++ components/lib/CoreValuesSection/CoreValuesSection.tsx
@@ -4,72 +4,90 @@ import { getConstants, getSeparateLicenses } from "@/services/services";
 import { useTranslation } from "react-i18next";
 
 const CoreValuesSection = () => {
-  const [coreValues, setCoreValues] = useState<any>({})
-  const [data, setData] = useState([])
+  const [coreValues, setCoreValues] = useState<any>({});
+  const [data, setData] = useState([]);
   const { t } = useTranslation("coreValuesSection");
 
   const getAllLicenes = () => {
     getSeparateLicenses().then((res) => {
-      setData(res.data)
-    })
-  }
+      setData(res.data);
+    });
+  };
 
-  console.log(data, 'data');
+  console.log(data, "data");
 
   const getCoreValues = () => {
     getConstants("core-values").then((res) => {
       setCoreValues(res.data);
     });
-  }
+  };
 
   useEffect(() => {
-    getCoreValues()
-    getAllLicenes()
-  }, [])
+    getCoreValues();
+    getAllLicenes();
+  }, []);
 
   return (
     <>
-    <div className="bg-[#00517b]">
-      <div className="container mx-auto px-12 py-16">
-        <div style={{
-        backgroundImage: 'url(/Icon_Settings.svg)',
-        backgroundRepeat: 'no-repeat',
-        width: '165px',
-        height: '165px',
-        position: 'absolute',
-        opacity: '0.5'
-      }}></div>
+      <div className="bg-[#00517b]">
+        <div className="container mx-auto px-12 py-16">
+          <div
+            style={{
+              backgroundImage: "url(/Icon_Settings.svg)",
+              backgroundRepeat: "no-repeat",
+              width: "165px",
+              height: "165px",
+              position: "absolute",
+              opacity: "0.5",
+            }}
+          ></div>
 
-        <p className="text-[#ffffff] text-[18px] font-semibold flex gap-4 mt-20">
+          <p className="text-[#ffffff] text-[18px] font-semibold flex gap-4 mt-20">
+            <Image
+              src={"yellowLine.svg"}
+              alt={"whatwedo"}
+              height={1}
+              width={40}
+            />
+            {t("coreValues")}
+          </p>
+          <p className="text-[#ffffff] text-[45px] font-bold mb-8">
+            {t("desc")}
+          </p>
+
+          <p
+            className="text-[#ffffff] text-[22px] font-normal"
+            dangerouslySetInnerHTML={{ __html: coreValues.description }}
+          />
+        </div>
+      </div>
+      <div className="container mx-auto px-12 py-16">
+        <p className="text-[#000000] text-[18px] font-semibold flex gap-4">
           <Image
             src={"yellowLine.svg"}
             alt={"whatwedo"}
             height={1}
             width={40}
           />
-        {t('coreValues')}
-        </p>
-        <p className="text-[#ffffff] text-[45px] font-bold mb-8">
-        {t('desc')}
-        </p>
-
-        <p className="text-[#ffffff] text-[22px] font-normal" dangerouslySetInnerHTML={{__html:coreValues.description}} />
-      </div>
-    </div>
-        <div className="container mx-auto px-12 py-16">
-        <p className="text-[#000000] text-[18px] font-semibold flex gap-4">
-          <Image src={"yellowLine.svg"} alt={"whatwedo"} height={1} width={40} />
           {t("licencesCaption")}
         </p>
-        <p className="text-[#000000] text-[45px] font-bold mb-8">{t("descLic")}</p>
-  
+        <p className="text-[#000000] text-[45px] font-bold mb-8">
+          {t("descLic")}
+        </p>
+
         <div className="lg:flex md:flex lg:justify-start md:justify-start justify-center grid gap-10">
-          {
-            data.map((item: any) => <Image key={item.id} src={item.imageUrl} alt={"iso"} width={200} height={200} />)
-          }
+          {data.map((item: any) => (
+            <Image
+              key={item.id}
+              src={item.imageUrl}
+              alt={"iso"}
+              width={200}
+              height={200}
+            />
+          ))}
         </div>
       </div>
-      </>
+    </>
   );
 };
 
