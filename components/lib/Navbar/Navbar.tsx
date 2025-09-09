a1785b96ccc87b21bfd358ab0f4a348b8734fe5f
dArk10r4 <goylerov04@gmail.com>
2025-09-09 16:35:35 +0400

    Change size of logo

--- components/lib/Navbar/Navbar.tsx
+++ components/lib/Navbar/Navbar.tsx
@@ -56,7 +56,7 @@ export default function Example() {
       >
         <div className="flex lg:flex-1">
           <Link href="/" className="-m-1.5 p-1.5">
-            <Image src={"/afmpro-logo.jpeg"} alt="Logo" width={85.08} height={41.41} />
+            <Image src={"/afmpro-logo.jpeg"} alt="Logo" width={200} height={41.41} />
           </Link>
         </div>
         <div className="flex lg:hidden gap-4">
