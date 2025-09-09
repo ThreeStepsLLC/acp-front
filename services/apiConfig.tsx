cb0406b4a9c0ddc5be06c56cee63f5bf05d13737
dArk10r4 <goylerov04@gmail.com>
2025-09-09 13:36:31 +0400

    make http

--- services/apiConfig.tsx
+++ services/apiConfig.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.afmpro.az/api/v1/",
+  baseURL: "http://api.afmpro.az/api/v1/",
 });
 
 api.interceptors.request.use((config: any) => {
