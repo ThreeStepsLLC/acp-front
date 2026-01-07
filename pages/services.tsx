c7631a1807e11a675e55a0b176c744ce4e3f0a96
FeridSadiqzada <feridsadiqzade@gmail.com>
2026-01-07 10:18:56 +0400

    Update services.tsx

--- pages/services.tsx
+++ pages/services.tsx
@@ -2,9 +2,19 @@ import Layout from "@/components/layout/Layout";
 import PageHeader from "@/components/lib/PageHeader";
 import ServicesSection from "@/components/lib/ServicesSection";
 import { useTranslation } from "react-i18next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Services() {
   const { t } = useTranslation("servicesPage");
+  const router = useRouter();
+  const [highlightedServiceId, setHighlightedServiceId] = useState<string | undefined>();
+
+  useEffect(() => {
+    if (router.isReady && router.query.service) {
+      setHighlightedServiceId(router.query.service as string);
+    }
+  }, [router.isReady, router.query.service]);
 
   return (
     <Layout title="Services">
@@ -12,7 +22,7 @@ export default function Services() {
         title={t("services")}
         subTitle={t("ourServices")}
       />
-      <ServicesSection showDescription={true} />
+      <ServicesSection showDescription={true} highlightedServiceId={highlightedServiceId} />
     </Layout>
   );
 }
