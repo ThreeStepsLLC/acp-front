95f5d37fcefc134e84dd3639d9a4401e16b927b7
yusifhasanov <yusif.hasanov17@gmail.com>
2026-01-21 10:10:32 +0400

    change domain names

--- next.config.js
+++ next.config.js
@@ -6,6 +6,20 @@ module.exports = {
         appDir: true
     },
     images: {
-        domains: ['cdn.az-ko.az'],
+        domains: ['cdn.az-ko.az', 'api.az-ko.az'],
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '8080',
+                pathname: '/media/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'api.az-ko.az',
+                pathname: '/**',
+            },
+        ],
+        unoptimized: process.env.NODE_ENV === 'development',
     },
 }
