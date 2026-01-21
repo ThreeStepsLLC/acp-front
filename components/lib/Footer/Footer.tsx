9d075e167cad551ea72079652c4130cbd68f89db
yusifhasanov <yusif.hasanov17@gmail.com>
2026-01-21 10:36:26 +0400

    change logo

--- components/lib/Footer/Footer.tsx
+++ components/lib/Footer/Footer.tsx
@@ -35,7 +35,7 @@ const Footer = () => {
     >
       <div className="grid lg:grid-cols-3 sm:grid-cols-2 gap-12">
         <div>
-          <Image src={"/azko-logo.jpeg"} alt="Logo" width={200} height={41.41} />
+          <Image src={"/azko-logo.png"} alt="Logo" width={200} height={41.41} />
           <p className="text-[16px] mt-[20px]" dangerouslySetInnerHTML={{ __html: values.description }} />
         </div>
         <div>
